5300190996efa9b5f0f7c117f927bc4e5049d92a
unknown <frankli@D-SJC-00552087.corp.ebay.com>
2014-10-29 14:23:50 -0700

    update

--- appVers_GMB_func.R
+++ appVers_GMB_func.R
@@ -6,6 +6,6 @@ maxDate <- max(df$created_dt)
 platGMB_bar <- ggplot(plat, aes(x = Platform, y = gmb)) 
 platGMB_bar <- platGMB_bar + geom_bar(stat="identity")
 platGMB_bar <- platGMB_bar + xlab("Platform")+ ylab("GMB") + title("GMB by Platform, Trailing 30 days")
+platGMB_bar <- platGMB_bar + theme_minimal()
 platGMB_bar
-
 #show percentages
