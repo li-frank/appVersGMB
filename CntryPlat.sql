98ce09d2a7edb1a0018e1e06788c18c56328dabd
unknown <frankli@D-SJC-00552087.corp.ebay.com>
2014-11-03 17:29:56 -0800

    commit

---
+++ CntryPlat.sql
@@ -0,0 +1,53 @@
+SELECT
+co.created_dt,
+CASE WHEN apps.app_id IS NULL AND sess_cobrand IN 7 THEN 'FSoM'  
+WHEN apps.app_id IS  NULL THEN 'Core Site on PC'
+WHEN TRIM(apps.prdct_name) IN 'IPhoneApp' THEN 'iPhone App'
+WHEN TRIM(apps.prdct_name) IN ('Android','Android Motors') THEN 'Android App'
+WHEN TRIM(apps.prdct_name) IN 'IPad' THEN 'iPad App'
+WHEN TRIM(apps.prdct_name) IN ('MobWeb','MobWebGXO') THEN 'Mobile Web'
+ELSE 'Other'
+END Platform,  
+
+CASE WHEN buyer_country_id IN  (1, -1, 0, -999, 225, 679, 1000) THEN 'US'  
+WHEN buyer_country_id IN 3 THEN 'UK'
+WHEN buyer_country_id IN 77 THEN 'DE'
+WHEN buyer_country_id IN 15 THEN 'AU'
+WHEN buyer_country_id IN 2 THEN 'CA'
+ELSE 'Other'
+END buyer_country,  
+
+SUM(CAST(co.item_price * co.quantity * CAST(cr.CURNCY_PLAN_RATE AS FLOAT) AS DECIMAL(18,2))) AS gmb_plan   
+
+FROM  
+(
+  select 
+  created_dt,
+  sess_cobrand,
+  buyer_id,
+  quantity,
+  item_price,
+  lstg_curncy_id,
+  leaf_categ_id,
+  app_id,
+  item_site_id,
+  buyer_country_id
+  
+  from p_soj_cl_v.checkout_metric_item
+  
+  where
+  created_dt BETWEEN current_date-93 AND current_date-2
+  AND auct_end_dt >= current_date-93
+  AND ck_wacko_yn = 'N'
+  AND auct_type_code NOT IN (12,15)   
+) co 
+
+INNER JOIN access_views.ssa_curncy_plan_rate_dim cr ON (co.lstg_curncy_id = cr.curncy_id)
+INNER JOIN access_views.dw_category_groupings cat ON (co.leaf_categ_id = cat.leaf_categ_id AND co.item_site_id = cat.site_id)
+LEFT JOIN access_views.dw_api_mbl_app apps ON (co.app_id = apps.app_id)
+
+WHERE  
+cat.sap_category_id NOT IN (5,7,23,41)
+
+GROUP BY  
+1,2,3;
--- appVers_GMB_clean.R
+++ appVers_GMB_clean.R
@@ -1,7 +1,7 @@
-#if restart script from just here
+#if restart, script from just here
 df<-df2
 
-#change <NA> to Unknown
+#change <NA>s to Unknown
 df[is.na(df)] <- 'Unknown'
 
 #see unique platforms
@@ -11,7 +11,6 @@ unique(df$Platform)
 df$created_dt <- as.Date(df$created_dt); head(df$created_dt)
 minDate <- min(df$created_dt); minDate
 maxDate <- max(df$created_dt); maxDate
-
 days <- as.numeric(maxDate-minDate+1); days
 
 #last 30 days' platform summary
@@ -19,7 +18,7 @@ plat <- ddply(df,.(Platform),summarize,gmb=sum(gmb_plan)/days)
 ##sort by GMB
 plat$Platform <-factor(plat$Platform, levels=plat[order(plat$gmb,decreasing=TRUE),"Platform"])
 
-#platforms over time
+#platforms over time (global)
 platDate <- ddply(df,.(Platform, created_dt),summarize,gmb=sum(gmb_plan))
 platDateShare <- ddply(platDate,"created_dt",transform,gmbDateShare=gmb/sum(gmb))
 ##sort Platform by GMB
@@ -27,12 +26,38 @@ platDateShare <- platDateShare[with(platDateShare,order(gmb)),]
 ##sort Platform factor by GMB (for legend)
 platDateShare$Platform <-factor(platDateShare$Platform, levels=platDateShare[order(platDateShare$gmb,decreasing=TRUE),"Platform"])
 
-#app verions over time (only iPhone & Android)
+#app verions over time
 appVers <- ddply(df,.(Platform,appVersion,created_dt),summarize,gmb=sum(gmb_plan))
 appVersShare <- ddply(appVers,.(created_dt,Platform),transform,gmbDateShare=gmb/sum(gmb))
 ##sort by GMB for filling & then legend
 #appVersShare <- appVersShare[with(appVersShare,order(appVersion,decreasing=TRUE)),]
 appVersShare$appVersion <-factor(appVersShare$appVersion, levels=appVersShare[order(appVersShare$gmb,decreasing=TRUE),"appVersion"])
+
+########################################################################
+#use function to repeat splicing on platforms
+trm <- function(toTrim){
+  return(gsub(" ","",toTrim))
+}
+
+clean <- function(plat){
+  plat0 <- trm(plat)
+  #create version shares
+  VersShare <- appVersShare[appVersShare$Platform==plat,]
+  #cumulative GMB
+  VersCum <- ddply(VersShare,.(appVersion),summarize,gmb=sum(gmb))
+  topVers <- VersCum[rev(order(VersCum$gmb)),"appVersion"][1:8]
+  VersShare <- VersShare[VersShare$appVersion %in% topVers,]
+  
+  return(VersShare)
+  #paste(plat,VersCum)<-ddply(appVers,.(appVersion),summarize,gmb=sum(gmb))
+}
+
+ipadShare <- clean('iPad App')
+iphoneShare <- clean('iPhone App')
+androidShare <- clean('Android App')
+mWebShare <- clean('Mobile Web')
+
+########################################################################
 ##separate sets for iPhone and Android
 iphoneVersShare <- appVersShare[appVersShare$Platform=="iPhone App",]
 androidVersShare <- appVersShare[appVersShare$Platform=="Android App",]
@@ -49,15 +74,15 @@ androidVersShare <- androidVersShare[androidVersShare$appVersion %in% topandroid
 
 ##sort versions and to eliminate stacked line swapping
 
-##############################
-#app versions over time
-platDate_AppVers <- ddply(df,.(Platform, created_dt, appVersion),summarize,gmb=sum(gmb_plan))
-
-#platform over time & country
-platDate_Country <- ddply(df,.(Platform, created_dt, buyer_country),summarize,gmb=sum(gmb_plan))
-
-#app & platform versions over time & country
-platDate_appVersCountry <- ddply(df,.(Platform, created_dt, appVersion, buyer_country),summarize,gmb=sum(gmb_plan))
+# ##############################
+# #app versions over time
+# platDate_AppVers <- ddply(df,.(Platform, created_dt, appVersion),summarize,gmb=sum(gmb_plan))
+# 
+# #platform over time & country
+# platDate_Country <- ddply(df,.(Platform, created_dt, buyer_country),summarize,gmb=sum(gmb_plan))
+# 
+# #app & platform versions over time & country
+# platDate_appVersCountry <- ddply(df,.(Platform, created_dt, appVersion, buyer_country),summarize,gmb=sum(gmb_plan))
 
 ##iPhone
 
